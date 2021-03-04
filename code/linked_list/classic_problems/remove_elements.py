0733354c25d057d002928efba7b32ef1cca2ce8e
moonorange <monoma632@gmail.com>
2021-03-04 23:32:05 +0900

    add: remove elements dummy head solution

--- code/linked_list/classic_problems/remove_elements.py
+++ code/linked_list/classic_problems/remove_elements.py
@@ -1,7 +1,8 @@
 # https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+from linked_list.utils import ListNode
 
 class Solution:
-    def removeElements(self, head: ListNode, val: int) -> ListNode:
+	def removeElements(self, head: ListNode, val: int) -> ListNode:
 		while (head and head.val == val):
 			head = head.next
 		if (head is None):
@@ -13,3 +14,14 @@ class Solution:
 			else:
 				head = head.next
 		return head_tmp
+
+	def removeElements_2(self, head: ListNode, val: int) -> ListNode:
+		dummy_head = ListNode(-1)
+		dummy_head.next =  head
+		tmp_head = dummy_head
+		while (dummy_head.next):
+			if (dummy_head.next.val == val):
+				dummy_head.next = dummy_head.next.next
+			else:
+				dummy_head = dummy_head.next
+		return tmp_head.next
